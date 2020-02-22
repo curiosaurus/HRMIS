4d7ee108d47ef332ebcdfa0f5a4b44fa556d4f6a
Gaurav Medhane <48349916+gaurav1502@users.noreply.github.com>
2020-02-22 20:27:06 +0530

    File
    upload

---
+++ file_m.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<script src="../js/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+	<div class="container">
+<hr>
+		<div class="text-center">
+		<?php 
+				require_once "vendor/autoload.php";
+				$client 	= new MongoDB\Client;
+				$dataBase 	= $client->selectDatabase('companydb');
+				$collection = $dataBase->selectCollection('upload');
+
+	//        In this block access by session variable not by id
+	
+
+				$cursor = $collection->find(array('id' => '3'));
+				foreach ($cursor as $obj) {
+                  ?>
+					 <html>
+					 
+					 <embed src="<?php print_r($obj["fileName"]);?>" type="application/pdf" width="100%" height="600px" />
+		             <img src="<?php print_r($obj["fileName"]);?>" height="100px" weight="100px">
+		 </img>
+		 </html>
+							
+					<?php	  
+				}
+				
+				if(isset($_POST['create'])) {
+			
+					if($_FILES['file']) {
+						if(move_uploaded_file($_FILES['file']['tmp_name'], 'upload/'.$_FILES['file']['name'])) {
+							// give session variable and pass it to dataBase
+							$data['fileName'] = 'upload/'.$_FILES['file']['name'];
+						
+						} else {
+							echo "Failed to upload file.";
+						}
+					}
+
+					$result = $collection->insertOne($data);
+
+					if($result->getInsertedCount()>0) {
+						echo "file is uploaded";
+					} else {
+						echo "File in not uploaded";
+					}
+				}?>
+		</div>
+		<div class="row">
+		    <div class="col-md-4">
+			    <form class="form-horizontal" method="post" action="" enctype="multipart/form-data">
+					<fieldset>
+						
+
+						<!-- File input-->
+						<div class="form-group" id="fileInput">
+						  <label class="col-md-12" for="file">Select Image</label>  
+						  <div class="col-md-12">
+						  <input id="file" name="file" type="file" placeholder="" class="form-control input-md">
+						  </div>
+						</div>
+
+
+						<button id="create" name="create" class="btn btn-primary">Create Article</button>
+						
+					</fieldset>
+				</form>
+		    </div>
+	</div>
+</body>
+</html>
