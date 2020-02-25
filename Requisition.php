fffb845be3c43d8277315b038641b0f8f373d7a6
Nishad Patil <nishadpatil63@gmail.com>
2020-02-25 20:57:20 +0530

    Add files via upload

--- Requisition.php
+++ Requisition.php
@@ -6,8 +6,42 @@
     $companydb = $client->companydb;
     $empcollection = $companydb->user;
 
+
+
+
+/**
+ * Creating MongoDB like ObjectIDs.
+ * Using current timestamp, hostname, processId and a incremting id.
+ * 
+ * @author Julius Beckmann
+ */
+function createMongoDbLikeId($timestamp, $hostname, $processId, $id)
+{
+	// Building binary data.
+	$bin = sprintf(
+		"%s%s%s%s",
+		pack('N', $timestamp),
+		substr(md5($hostname), 0, 3),
+		pack('n', $processId),
+		substr(pack('N', $id), 1, 3)
+	);
+
+	// Convert binary to hex.
+	$result = '';
+	for ($i = 0; $i < 12; $i++) {
+		$result .= sprintf("%02x", ord($bin[$i]));
+	}
+
+	return $result;
+}
+
+
     if(isset($_POST['submit']))
-    {
+    {   
+        foreach(range(0, 0) as $id) {
+        $id = 7841;
+        }
+        $unique_id = createMongoDbLikeId(time(), php_uname('n'), getmypid(), $id); 
         $department = $_POST['department'];
         $reqfor = $_POST['reqfor']; 
         $reasonappnt = $_POST['reasonappnt'];
@@ -25,16 +59,15 @@
         $reqiso = $_POST['reqiso'];
         $actiso = $_POST['actiso'];
     // Insert one data
-    $insertOneResult = $empcollection->insertOne( ['department' => $department ,'position' => $reqfor , 'reasonofappointment' => $reasonappnt , 'dateofcreation' => $dateofcreation , 'minqual' => $minqual , 'prefqual' => $prefqual , 'minexp' =>  $expmin , 'prefexp' => $expmax , 'skillreq' => $skillsreq , 'spconsideration' =>  $skillconsider , 'Communication Skill REQUIRED' => $reqcomm , 'Communication Skill ACTUAL' => $actcomm , 'Vendor Selection & Assessment REQUIRED' => $reqven , 'Vendor Selection & Assessment ACTUAL' => $actven , 'ISO REQUIRED' =>  $reqiso , 'ISO ACTUAL' => $actiso ] );
+    $insertOneResult = $empcollection->insertOne( ['unique_id' => $unique_id , 'department' => $department ,'position' => $reqfor , 'reasonofappointment' => $reasonappnt , 'dateofcreation' => $dateofcreation , 'minqual' => $minqual , 'prefqual' => $prefqual , 'minexp' =>  $expmin , 'prefexp' => $expmax , 'skillreq' => $skillsreq , 'spconsideration' =>  $skillconsider , 'Communication Skill REQUIRED' => $reqcomm , 'Communication Skill ACTUAL' => $actcomm , 'Vendor Selection & Assessment REQUIRED' => $reqven , 'Vendor Selection & Assessment ACTUAL' => $actven , 'ISO REQUIRED' =>  $reqiso , 'ISO ACTUAL' => $actiso ] );
 
-    if($insertOneResult)
-    {
-        echo "Sucess";
-    }
-    else{
-        echo "unSucess";
-
-    }
+    // if($insertOneResult)
+    // {
+    //     echo "Sucess";
+    // }
+    // else{
+    //     echo "unSucess";
+    // }
     }
 ?>
 
