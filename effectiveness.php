57156f50fbb494dfecd48bb738a654c9b026d8f1
Kedar Parsewar <42874351+kedar1023@users.noreply.github.com>
2020-03-12 12:45:45 +0000

    Add files via upload

---
+++ effectiveness.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TRAINNING EFFECTIVENESS
+    </title>
+    <!-- Google font cdn file imported here -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- bootstrap cdn files for the Tables and other contents  -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Link the External Css here And please see name Its a Styles.css  -->
+    <link rel="stylesheet" href="styles.css">
+
+    <style>
+        
+    </style>
+</head>
+
+
+<body>
+    <div class="p">
+        <!-- NavBar for the Logo and the title -->
+        <div class="nav">
+            <div class="logo">
+                <span>LOGO</span>
+            </div>
+            <center><span class="hr"></span></center>
+            <hr>
+        </div>
+        <hr>
+        <!-- Logo and the title navbar close here -->
+
+
+
+        <!-- Main navbar that contains home recruittment Should Be start here -->
+        <nav class="shadow p-3 mb-5 bg-white rounded">
+            <div class="gri">
+                <div class="item1">
+                    <a href="#"> HOME</a>
+                </div>
+                <div class="item2">
+                    <a href="#"> EMPLOYEE MASTER	</a>
+
+
+                </div>
+                <div class="item3">
+                    <div class="dropdown">
+                        <span> <a href="#"> TRAINNING</a></span>
+                        <div class="dropdown-content">
+                            <p>First</p>
+                            <p>Secound</p>
+                            <p>Third</p>
+                        </div>
+                    </div>
+
+
+
+                </div>
+                <div class="item4">
+                    <a href="#"> PMS	</a>
+
+
+                </div>
+                <div class="item5">
+                    <a href="#"> WELFARE	</a>
+
+
+                </div>
+                <div class="item6">
+
+                </div>
+                <div class="item7">
+                    <a href="#"> COMPANY</a>
+                </div>
+        </nav>
+        </div>
+    </div>
+    <!-- Main navbar Close here -->
+
+
+    <div class="title">
+        <center>
+           <h5>(Part-III)</h5>
+        </center>
+    </div>
+
+
+<br>
+<center>
+    <div  style="width: 1140px;border: 1px solid royalblue; ;">
+        <center>To be filled in after 1 Month of tranning</center>
+        </div></center>
+<div class="container" style="border: 1px solid lightblue; padding: 25px;">
+
+<br>
+<center><h5>TRAINNING EFFECTIVENESS</h5></center>
+<br><br>
+<table class="table" align="center">
+<tr>
+   <td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+       
+    
+    
+    Date : <input type="text"></td>
+    <td>Skill level before tranning <input type="text">
+    <br>
+   <b>Legend:</b> 
+    </td>
+</tr>
+
+
+<tr>
+    <td> &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        
+     
+     
+     Venue : <input type="text">
+    <br><br><br>
+    &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+    Subject : <input type="text">
+    </td>
+     <td>
+         <table class="table" border="1">
+             <tr>
+                 <td>tranning Required</td>
+                 <td>0</td>
+             </tr>
+             <tr>
+                 <td>Work can be assign under Supervision</td>
+             <td>1</td>
+                </tr>
+
+                
+             <tr>
+                <td>Capable to work indivisully</td>
+            <td>2</td>
+               </tr>
+
+               
+             <tr>
+                <td>Capable to work indivisully & train others</td>
+            <td>3</td>
+               </tr>
+         </table> 
+     </td>
+ </tr>
+
+<tr>
+    <td> 
+        &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        Duration : <input type="text" name="" id=""> </td>
+    <td>
+    </td>
+</tr>
+
+<tr>
+    
+    <td>
+        &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        Faculty : <input type="text" name="" id=""> </td>
+    <td>
+    </td>
+</tr>
+
+
+<tr>
+    
+    <td>
+        &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        Trainee Name : <input type="text" name="" id=""> </td>
+    <td>
+        Employee No. <input type="text" name="" id="">
+    </td>
+</tr>
+
+
+</table>
+
+</div>
+
+<br><br>
+
+<center>
+    <div  style="width: 1140px;border: 1px solid royalblue; ;">
+        <center> 
+            (Review done by HOD)
+        </div></center>
+<div class="container" style="border: 1px solid lightblue; padding: 25px;">
+
+<br>
+
+<center>
+
+    1. Has the trainee implemented this in his/her working area?
+    <br><br>
+    Yes/No <input type="text">
+</center>
+
+<br>
+<center>
+
+    2. If yes, where give example or evidence or other specification
+    <br><br>
+    Yes/No <input type="text">
+</center>
+<br>
+
+<center>
+
+    3. are you satisfied with tranning? 
+    Yes/No 
+    <br><br>
+    a) If yes, please confirm trainee Skill level after tranning (_______) <br>
+    <input type="text">
+
+    <br><br>
+    
+    b) If No, why
+    <input type="text">
+</center>
+
+</div>
+
+<br><br>
