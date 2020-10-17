97893597f50a088bfd947057c08c3ceaabe1d4f7
Pratik Patil <pratikdpatil2000@gmail.com>
2020-10-18 01:46:10 +0530

    Add files via upload

--- skillmatrixlist.php
+++ skillmatrixlist.php
@@ -1,17 +1,11 @@
-
 <?php
-
 require 'vendor\autoload.php'; 
-
 $client = new MongoDB\Client;
-$companydb = $client->companydb;
+$companydb = $client->hrmis;
 $empcollection = $companydb->user;
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,86 +22,62 @@ $empcollection = $companydb->user;
         th {
             border: 1px solid black;
             width: 500px;
-        }
-        
+        }        
         .table2 {
             border: 1px solid black;
             width: 500px;
         }
-        
         .table3 tr th td {
             border: 1px solid black;
             width: 100%;
         }
-        
         a {
             text-decoration: none;
         }
     </style>
 </head>
-
 <body>
 <?php
-
     include 'hodnavbar.php';
-
 ?>
     <center>
-
         <h1>SKILL MATRIX</h1>
         <br>
     </center>
     <div class="container" style="border: 1px solid lightblue; padding: 2px;">
-
         <div class="row justify-content-md-around">
-
             <div class="col-md-0">
                 <h4><label> Year</label></h4>
             </div>
-
-
             <div class="col-md-8">
-                <select required class="form-control form-control-lg">
-                    <option>YEAR JANUARY 2019 TO DECEMBER 2019</option>
-                    <option>YEAR JANUARY 2019 TO DECEMBER 2019</option>
-                    <option>YEAR JANUARY 2019 TO DECEMBER 2019</option>
-                    <option>YEAR JANUARY 2019 TO DECEMBER 2019</option>
+                <select name="year" id="year" onchange="pp();" required class="form-control form-control-lg">
+                    <?php  
+                    $years=$companydb->years;
+                    $counter = $years->find();
+                    foreach($counter as $row) {
+                        echo "<option value = ".$row['year'].">". $row['year'] ."</option>";
+                    }
+                    ?>
                   </select>
             </div>
-
         </div>
     </div>
     <br><br>
     <div class="container" style="border: 1px solid lightblue; padding: 2px;">
-
         <div class="row justify-content-md-around">
-
-            <!-- <div class="col-md-0">
-                <h4><label> DEPARTMENT</label></h4>
-            </div>
-
-
-            <div class="col-md-8">
-                <h4><label>ACCOUNTS & EXCISE</label></h4>
-            </div> -->
   <?php
     echo '<div class="col-md-3"><div class="dropdown">';
-    $masteropt='masteropt';
-    $client = new MongoDB\Client;
-    $companydb = $client->hrmis;
-    $empcollection = $companydb->$masteropt;
-    $counter = $empcollection->find(['type'=>'department']);
+    $masteropt = $companydb->masteropt;
+    $counter = $masteropt->find(['type'=>'department']);
     echo'<select name="department" id="department" onchange="pp();">';
-    if(!isset ($_GET['uid'])){
+    if(isset ($_GET['uid'])){
     foreach($counter as $row) {
         if($_GET["uid"] == $row['value']){
         echo "<option value = ".$row['value']." selected>". $row['value'] ."</option>";
-        $deptid=$row['value'];
-        
+        $deptid=$row['value']; 
     }
         else{
             echo "<option value = ".$row['value']." >". $row['value'] ."</option>";
-
         }
     }
 }
@@ -117,23 +87,18 @@ $empcollection = $companydb->user;
             $deptid=$row['value'];
         }   
     }
-    
 echo '</select>';
-    
 ?>
 <script>
 function pp(){
     var p = document.getElementById("department").value;
-    window.location.href="skillmatrixlist.php?uid="+p;
+    var y = document.getElementById("year").value;
+    window.location.href="skillmatrixlist.php?uid="+p+"&year="+y;
 }
-
 </script>
         </div>
     </div>
-
-
     <br><br>
-
     <table class="table3" border="2" style="width:100%">
     <thead>    
     <tr>
@@ -145,43 +110,37 @@ function pp(){
             <th>Total Experience</th>
             <th>Skill Matrix</th>
         </tr>
-        
-    </thead>
-    
+    </thead>    
     <tbody>
-
+<?php
         //database retrive
-        $companydb = $client->companydb;
-        $empcollection = $companydb->user;
-        $counter = $empcollection->find(['DEPARTMENT ID'=>$deptid]);
+        //echo "lol";
+        $y=$_GET['year'];
+        $empcollection = $companydb->empcollection;
+        $counter = $empcollection->find(['Department Id'=>$deptid]);
         foreach($counter as $row) {
-    
-    // echo $var;
-    $pas=$row['Emp code']
+    //echo $row;
+    //echo "lol";
+    $pas=$row['Emp Code'];
     echo "<tr>";
     echo "<td>" . $row['Emp Code'] ."</td>";
     echo "<td>" . $row['Emp Display Name'] ."</td>";
+    echo "<td>" . $row['Designation'] ."</td>";
+    echo "<td>" . $row['Grade Id'] ."</td>";
+    echo "<td>" . $row['EDUCATION'] ."</td>";
     echo "<td>" . $row['TOTAL EXP'] ."</td>";
-    echo "<td>" . $row['GRADE ID'] ."</td>";
-    echo "<td>" . $row['DEPARTMENT ID'] ."</td>";
-    echo "<td><a href='skillmatrix.php?variable1=".$pas."'>Upload</a>" ."</td>";
+    echo "<td><a href='skillmatrix.php?variable1=".$pas."&year=".$y."'>Upload</a>" ."</td>";
     #add just this line whenever you create  viewrequisition  33111`3
     //getting values in page2.php file by $_GET function:
     //$x=$_GET['variable1'];
     echo "</tr>";
 }
 ?>
-
     </tbody>
-
-
     </table>
     <br>
-
-
-    <center>
+    <!-- <center>
         <a href="update.html"><button name="" class="btn btn-block btn-primary">  SUBMIT</button></a>
-    </center>
+    </center> -->
 </body>
-
 </html>
