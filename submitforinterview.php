814a0a6d78534e2b9dfefdeed001b7c113f25008
Pratik Patil <pratikdpatil2000@gmail.com>
2020-10-19 10:53:19 +0530

    Add files via upload

---
+++ submitforinterview.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor\autoload.php'; 
+$client = new MongoDB\Client;
+$companydb = $client->hrmis;
+$empcollection = $companydb->requisition;
+    if (isset($_GET['req'])){
+        $reqid=$_GET['req'];
+        $d=date("Y/m/d");
+        $updateResult = $empcollection->updateOne(
+            ['unique_id' => $reqid],
+            ['$set' => ['status' => 'Candidate_Short_listed','dateofhodshortlist'=>$d]]
+        );
+    }
+    header('Location:open_positions.php');
+?>
