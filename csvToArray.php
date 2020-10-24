c5daf21aa0611cc3fbe6c71d8208d19edcff9aee
Shyam Pandav <46902914+pandavshyam@users.noreply.github.com>
2020-10-25 00:02:08 +0530

    Add files via upload

---
+++ csvToArray.php
@@ -0,0 +1,22 @@
+<?php
+
+$array = $fields = array(); $i = 0;
+$handle = @fopen("file.csv", "r");
+if ($handle) {
+    while (($row = fgetcsv($handle, 4096)) !== false) {
+        if (empty($fields)) {
+            $fields = $row;
+            continue;
+        }
+        foreach ($row as $k=>$value) {
+            $array[$i][$fields[$k]] = $value;
+        }
+        $i++;
+    }
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($handle);
+}
+
+?>
